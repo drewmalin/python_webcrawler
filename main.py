f13d297668a398a8efd2905287739856ae846558
Drew Malin <drew.malin@gmail.com>
2014-04-20 19:26:02 -0700

    loading bars and logging and opt args oh my

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 """
     Web crawler!
 """
-import urllib
-import re
-import random
+import urllib, re, random, argparse, sys, time, json
 
 CORPUS = {}
-START_LINK = "http://www.drewmalin.com/"
-MAX_DEPTH = 1
+START_URL = ""
+MAX_DEPTH = 0
+VERBOSE = False
+CURRENT_PROGRESS = 0
 
 def generate_corpus(url, current_depth=0):
     """
@@ -27,9 +27,12 @@ def generate_corpus(url, current_depth=0):
     if url is None:
         return
 
-    print "[LOG]\tCrawling [" + url + "]..."
+    print "\n[LOG]\tCrawling [" + url + "]..."
+    update_progress(0)
     html = urllib.urlopen(url).read()
+    update_progress(.1)
     parse_links(html, url, current_depth)
+    update_progress(1)
 
 def parse_links(html, context_link, depth):
     """
@@ -42,20 +45,33 @@ def parse_links(html, context_link, depth):
         depth - Current depth from the beginning of execution
     """
     new_urls = []
-
-    for link in get_html_links(html):
+    links = get_html_links(html)
+    for link in links:
         title, url = parse_link(link)
-        
         url = validate_url(url, context_link)
+        
+        # This portion of the algorithm is considered to be 90% of the 
+        # processing. This 90% is broken into two chunks, each of which updates
+        # the progress bar by a fraction equal to 1/(the number of links to 
+        # process). At this point, half half of that portion of the 90% chunk 
+        # is complete.
+        update_progress(CURRENT_PROGRESS + .9 * float(.5)/len(links))
+
         if url is None:
             continue
 
         if persist_link(title, url):
-            print "[LOG]\t\tNew link found: (" + title + ") -> [" + url + "]"
+            if VERBOSE:
+                print "[DEBUG]\t\tNew link found: (" + \
+                    title + ") -> [" + url + "]"
             new_urls.append(url)
         else:
-            print "[DEBUG]\t\tSkipping known or invalid link: (" \
-                + title + ") -> [" + url + "]"
+            if VERBOSE:
+                print "[DEBUG]\t\tSkipping known or invalid link: (" \
+                    + title + ") -> [" + url + "]"
+
+        # The second half of the 90% chunk is now complete.
+        update_progress(CURRENT_PROGRESS + .9 * float(.5)/len(links))
 
     # Recurse on the newly found links (breadth-first)
     for url in new_urls:
@@ -75,8 +91,9 @@ def get_html_links(html):
 
     for location in link_locations:
         html_link = html[location[0]:location[1]]
-        html_link = re.sub('\s+', ' ', html_link)
-        html_links.append(html_link)
+        html_link = re.sub('\s+', ' ', html_link).strip()
+        if html_link:
+            html_links.append(html_link)
 
     return html_links
 
@@ -86,11 +103,26 @@ def parse_link(link):
         the link points to. If either one of these does not exist, None is returned in
         its place.
     """
-    # Grab the largest amount of text enclosed in an a tag,
-    # then strip the tag away.
-    link_title = re.search('>.*</a>', link).group(0)[1:-4]
-    # Grab the shortest amount of text enclosed in quotes after the href.
-    link_url = re.search('href=["\'].*?["\']', link).group(0)[6:-1]
+    if VERBOSE:
+        print "[DEBUG]\t\tParsing tag HTML: " + link
+    
+    # Grab the largest amount of text enclosed in an a tag, then strip the tag
+    # away. If no title is found, return None.
+    link_title_re = re.search('>.*</a>', link)
+    link_title = link_title_re.group(0)[1:-4] if link_title_re else None
+    # Grab the shortest amount of text after 'href='. Anchor tags *usually*
+    # have quote-wrapped links, but this is not required. The following are
+    # valid:
+    # <a href="url"></a>
+    # <a href='url'></a>
+    # <a href=url></a>
+    link_url_re = re.search('href=["\']?.*?["\'> ]', link)
+    link_url = link_url_re.group(0)[5:-1] if link_url_re else None
+    
+    if link_url and link_url[0] in ("\'", "\""):
+        link_url = link_url[1:]
+    if link_url and link_url[-1] in (" ", ">", "\'", "\""):
+        link_url = link_url[:-1]
 
     return link_title, link_url
 
@@ -107,7 +139,7 @@ def persist_link(title, url):
     if not title:
         return False
     # We've already seen this exact link? No need to save again.
-    if title in CORPUS and CORPUS[title] == url:
+    if url == START_URL or (title in CORPUS and CORPUS[title] == url):
         return False
     for key, value in CORPUS.iteritems():
         # We've already seen this url, but under a different key
@@ -149,25 +181,76 @@ def validate_url(url, context_link):
         url = url[:url.find("#")]
     out_url = url
     try:
-        response = urllib.urlopen(url)
+        _ = urllib.urlopen(url)
     except:
         try:
-            response = urllib.urlopen(context_link+url)
+            _ = urllib.urlopen(context_link+url)
         except:
             out_url = None
-            print "[DEBUG]\t\tInvalid URL! (" + url + ")"
+            if VERBOSE:
+                print "[DEBUG]\t\tInvalid URL! (" + url + ")"
         else:
             out_url = context_link+url
-            print "[DEBUG]\t\tFixing URL... " + \
-                  "\n\t\t\tfrom: " + url + \
-                  "\n\t\t\tto: " + out_url
+            if VERBOSE:
+                print "[DEBUG]\t\tFixing URL... " + \
+                    "\n\t\t\tfrom: " + url + \
+                    "\n\t\t\tto: " + out_url
     return out_url
 
-print "[INFO] START"
-generate_corpus(START_LINK)
-print "Found " + str(len(CORPUS)) + " unique links"
-print "{"
-for entry in CORPUS:
-    print "\t(" + entry + ")\n\t\t-> [" + CORPUS[entry] + "]"
-print "}"
-print "[INFO] SUCCESS"
+def update_progress(new_progress):
+    """
+        Update the progress bar status to the specified position. If the specified
+        position is greater than 1 (i.e., resulting in a progress greater than 100%)
+        then the bar is set to 100%. The progress bar will not be printed if verbose
+        mode is on (as the command line clutter looks very messy).
+    """
+    if VERBOSE:
+        return
+    global CURRENT_PROGRESS
+    CURRENT_PROGRESS = new_progress if new_progress <= 1 else 1
+    length = 20
+    progress = '#' * int(CURRENT_PROGRESS * length)
+    whitespace = ' ' * (length - len(progress))
+    sys.stdout.write("\r[{0}] {1}%".format(progress + whitespace, \
+                     int(CURRENT_PROGRESS * 100)))
+    sys.stdout.flush()
+
+
+def main():
+    """ 
+        Entry point into the crawler.
+    """
+    global VERBOSE
+    global MAX_DEPTH
+    global START_URL
+
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument("-v", "--verbose", 
+                            help="Enable verbose logging", action="store_true")
+    arg_parser.add_argument("-d", "--depth",
+                            help="Specify maximum search depth", type=int)
+    arg_parser.add_argument("url",
+                            help="URL on which to begin crawl", type=str)
+    arg_parser.add_argument("-o", "--output",
+                            help="File for final output", type=str)
+    args = arg_parser.parse_args()
+
+    VERBOSE = args.verbose
+    MAX_DEPTH = args.depth
+    START_URL = args.url
+    
+    print "[LOG] START"
+    start_time = time.time()
+    generate_corpus(START_URL)
+    print "\nFound " + str(len(CORPUS)) + " unique links in " + \
+        str(time.time() - start_time) + " seconds"
+    if args.output:
+        out_file = open(args.output, 'w')
+        json.dump(CORPUS, out_file, sort_keys=True, indent=4, 
+                  ensure_ascii=False)
+        out_file.close()
+        print "[LOG] Results written to " + args.output
+    print "[LOG] SUCCESS"
+
+if __name__ == "__main__":
+    main()
